55cc5836c89439b5332c0a7007e04fb534d35ac7
harkiran7237 <harkiran7237@gmail.com>
2020-04-12 08:28:23 +0530

    sketch

--- sketch.js
+++ sketch.js
@@ -1,29 +1,43 @@
-var ball;
+var ball,locNode;
+var database,position;
 
 function setup(){
+    database= firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var locNode=database.ref("Ball/Positions");
+    locNode.on("value",readOp,showErr);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("Ball/Positions").set({
+    'x': ball.x + x,
+    'y': ball.y + y
+    })
+}
+function readOp(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
+function showErr(){
+   // console.log(error);
 }
